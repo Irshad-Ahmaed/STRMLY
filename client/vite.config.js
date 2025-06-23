e027d3d6a0efe3fccdff97b8ab56ca8a01967956
IRSHAD AHMAD <129326052+Irshad-Ahmaed@users.noreply.github.com>
2025-06-23 23:04:28 +0530

    railway command

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
       "/api": {
         target: process.env.BACKEND_URL || "http://localhost:4000",
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
       },
     },
   },
